70f98c53b59ba365f4bdb4d6bb157ba3c43abf83
Oliferuk Ivan <oliferukivan19@gmail.com>
2026-09-11 17:27:32 +0300

    Lab1: додано клас SportTeam

---
+++ lab1v16/Program.cs
@@ -0,0 +1,50 @@
+﻿class SportTeam
+{
+    private string name;
+    private string coach;
+    private int playersCount;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public string Coach
+    {
+        get { return coach; }
+        set { coach = value; }
+    }
+
+    public int PlayersCount
+    {
+        get { return playersCount; }
+        set { playersCount = value; }
+    }
+
+    public SportTeam(string name, string coach, int playersCount)
+    {
+        this.name = name;
+        this.coach = coach;
+        this.playersCount = playersCount;
+    }
+
+    public void PlayMatch()
+    {
+        Console.WriteLine($"{name} під керівництвом тренера {coach} виходить на матч у складі {playersCount} гравців.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SportTeam team1 = new SportTeam("Динамо", "Іван Петренко", 22);
+        SportTeam team2 = new SportTeam("Шахтар", "Олег Коваль", 20);
+        SportTeam team3 = new SportTeam("Карпати", "Андрій Мельник", 21);
+
+        team1.PlayMatch();
+        team2.PlayMatch();
+        team3.PlayMatch();
+    }
+}
